bb325592c22be8f432518b3e08a5bb5e2d6b04e0
Yuri Vaz <yurigava@gmail.com>
2021-01-28 22:22:25 -0300

    Unifying shipping methods

--- excelFormatter.py
+++ excelFormatter.py
@@ -21,7 +21,8 @@ columns = [
     'email',
     'cpf',
     'conteudo',
-    'valor'
+    'dimensoes',
+    'peso'
 ]
 data = {pacName: OrderedDict([(name, []) for name in columns]),
         sedexName: OrderedDict([(name, []) for name in columns])}
@@ -44,7 +45,7 @@ with open('pedidos.csv', newline='', encoding='iso-8859-1') as csvfile:
         filteredLines = getRowByShipping(reader, shipType)
         for rowNum, row in enumerate(filteredLines):
             currentData['idPedido'].append(row[0])
-            currentData['nome'].append(row[27])
+            currentData['nome'].append(row[27].upper())
             currentData['endereco'].append(row[14])
             currentData['numero'].append(row[15])
             currentData['complemento'].append(row[16])
@@ -54,29 +55,46 @@ with open('pedidos.csv', newline='', encoding='iso-8859-1') as csvfile:
             currentData['cep'].append(row[20])
             currentData['email'].append(row[22])
             currentData['cpf'].append(row[23].split('"')[1])
-            currentData['valor'].append(row[6])
+            try:
+                currentData['dimensoes'].append([row[28], row[29], row[30]])
+                currentData['peso'].append(row[31])
+            except IndexError:
+                print('ERRO!!!!!!!:')
+                print(f'Dimensões e pesos não encontrados para pedido de {currentData["nome"][-1:]}'
+                      '\nOrdem => dimensões (menor para maior) e peso.')
+                input()
+                quit()
 
 with open('conteudo.csv', newline='', encoding='iso-8859-1') as csvfile:
     reader2 = csv.reader(csvfile, delimiter=';')
     for shipType in data:
         currentData = data[shipType]
+        if len(currentData['idPedido']) == 0:
+            continue
         currentData['conteudo'] = [getConteudo(currentId, csvfile, reader2) for currentId in
                                    currentData['idPedido']]
         wb = Workbook()
         ws = wb.active
-        for index in range(len(idPedido)):
-            ws.cell(column=1, row=index+1, value=nome[index])
-            ws.cell(column=2, row=index+1, value=cep[index])
-            ws.cell(column=3, row=index+1, value=endereco[index])
-            ws.cell(column=4, row=index+1, value=numero[index])
-            ws.cell(column=5, row=index+1, value=complemento[index])
-            ws.cell(column=6, row=index+1, value=bairro[index])
-            ws.cell(column=7, row=index+1, value=cidade[index])
-            ws.cell(column=8, row=index+1, value=estado[index])
-            ws.cell(column=12, row=index+1, value=email[index])
-            ws.cell(column=13, row=index+1, value=cpf[index])
-            ws.cell(column=27, row=index+1, value=valor[index])
-        wb.save(f'editado{shipType}.xlsx')
+        for index in range(len(currentData['idPedido'])):
+            rowNum = index + 1
+            ws.cell(column=1,  row=rowNum, value=currentData['nome'][index])
+            ws.cell(column=2,  row=rowNum, value=currentData['cep'][index])
+            ws.cell(column=3,  row=rowNum, value=currentData['endereco'][index])
+            ws.cell(column=4,  row=rowNum, value=currentData['numero'][index])
+            ws.cell(column=5,  row=rowNum, value=currentData['complemento'][index])
+            ws.cell(column=6,  row=rowNum, value=currentData['bairro'][index])
+            ws.cell(column=7,  row=rowNum, value=currentData['cidade'][index])
+            ws.cell(column=8,  row=rowNum, value=currentData['estado'][index])
+            ws.cell(column=12, row=rowNum, value=currentData['email'][index])
+            ws.cell(column=13, row=rowNum, value=currentData['cpf'][index])
+            ws.cell(column=19, row=rowNum, value=currentData['peso'][index])
+            ws.cell(column=20, row=rowNum, value=currentData['dimensoes'][index][2])
+            ws.cell(column=21, row=rowNum, value=currentData['dimensoes'][index][0])
+            ws.cell(column=22, row=rowNum, value=currentData['dimensoes'][index][1])
+        wb.save(f'editado-{shipType.upper()}.xlsx')
+
+with open('conteudo.pickle', 'wb') as conteudoDb:
+    pickle.dump(data, conteudoDb)
 
 #print(f'Nome: {nome[0]}')
 #print(f'CEP: {cep[0]}')
--- rodar.py
+++ rodar.py
@@ -4,71 +4,75 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import setupInfo
-from openpyxl import Workbook
-import csv
-from unidecode import unidecode
+import pickle
 import time
 
-idPedido = []
-nomeClientes = []
-listaConteudos = []
-valor = []
+idPedido = 'idPedido'
+nomeClientes = 'nome'
+listaConteudos = 'conteudo'
+pacName = 'pac'
+sedexName = 'sedex'
+
+dictKeys = [
+    idPedido,
+    nomeClientes,
+    listaConteudos
+]
+
 INDICE_DESC_PRODUTO = 0
 INDICE_QTY_PRODUTO = 1
 INDICE_VALOR_PRODUTO = 2
 
-def getConteudo(id, csv):
-    found = ([linha[3], linha[10], linha[11]] for linha in reader2 if linha[0] == id)
-    csv.seek(0)
-    return list(found)
+with open('conteudo.pickle', 'rb') as conteudoDb:
+    recoveredData = pickle.load(conteudoDb, encoding='bytes')
 
-with open('pedidos.csv', newline='', encoding='iso-8859-1') as csvfile:
-    reader = csv.reader(csvfile, delimiter=';')
-    for rowNum, row in enumerate(reader):
-        print(row)
-        if(rowNum != 0 and row[8] != 'Motoboy'):
-            idPedido.append(row[0])
-            nomeClientes.append(unidecode(row[27].upper()))
-            valor.append(row[6])
+pedidosInfo = {
+    key: [value for shipping in recoveredData for value in recoveredData[shipping][key]]
+    for key in dictKeys
+}
 
-with open('conteudo.csv', newline='', encoding='iso-8859-1') as csvfile:
-    reader2 = csv.reader(csvfile, delimiter=';')
-    listaConteudos = [getConteudo(id, csvfile) for id in idPedido]
+print(pedidosInfo[nomeClientes])
 
-print(nomeClientes)
-##Editar!
+
 def waitBy(byType, value):
     return WebDriverWait(browser, 20).until(
             EC.element_to_be_clickable((byType, value))
         )
 
+
 def clickByXpath(xpath):
     waitBy(By.XPATH, xpath)
     browser.find_element_by_xpath(xpath).click()
 
+
 def clickById(id):
     waitBy(By.ID, id)
     browser.find_element_by_id(id).click()
+
 
 def waitInvisibility(xpath):
     WebDriverWait(browser, 10).until(
         EC.invisibility_of_element_located((By.XPATH, xpath))
     )
 
+
 def waitTillPageLoads():
     waitInvisibility("//div[contains(@class, 'modal in')]/div/div/div/h3[text()='Aguarde...']")
     time.sleep(1)
     waitInvisibility("//div[contains(@class, 'modal in')]/div/div/div/h3[text()='Aguarde...']")
+
 
 def waitTillTableLoads():
     waitInvisibility("//div[@class='fixed-table-loading' and following-sibling::table[@id='TabelaPostagens']]")
     time.sleep(1)
     waitInvisibility("//div[@class='fixed-table-loading' and following-sibling::table[@id='TabelaPostagens']]")
 
+
 def fillInContentValue(xpath, valueText):
     browser.find_element_by_xpath(xpath).click()
     browser.find_element_by_xpath(xpath+"/input").clear()
     browser.find_element_by_xpath(xpath+"/input").send_keys(valueText)
+
 
 browser = Firefox()
 browser.get('https://vipp.visualset.com.br/vipp/inicio/index.php')
@@ -86,7 +90,7 @@ clickByXpath("(//th/div[contains(@class,'th-inner') and text()='Nº ViPP'])[1]")
 waitTillTableLoads()
 waitBy(By.XPATH, "(//button[contains(@class,'btnedtobj')])[1]")
 
-for index in range(len(idPedido)):
+for index in range(len(pedidosInfo[idPedido])):
     waitTillPageLoads()
     waitTillTableLoads()
     waitBy(By.XPATH, "(//button[contains(@class,'btnedtobj')])[1]")
@@ -96,7 +100,7 @@ for index in range(len(idPedido)):
     waitTillPageLoads()
 
     clienteNome = browser.find_element_by_id('txtNomeDestinatario').get_attribute("value")
-    clienteIndex = nomeClientes.index(clienteNome.upper())
+    clienteIndex = pedidosInfo[nomeClientes].index(clienteNome.upper())
     print(clienteIndex)
     waitBy(By.ID, "btnDeclaracaoConteudo")
     clickById("btnDeclaracaoConteudo")
@@ -104,8 +108,8 @@ for index in range(len(idPedido)):
 
     waitTillPageLoads()
     waitBy(By.XPATH, "(//div[contains(@class,'ui-widget-content')]/div[contains(@class, 'CssPes')])[1]")
-    for produtoIndex in range(len(listaConteudos[clienteIndex])):
-        currentConteudos = listaConteudos[clienteIndex]
+    for produtoIndex in range(len(pedidosInfo[listaConteudos][clienteIndex])):
+        currentConteudos = pedidosInfo[listaConteudos][clienteIndex]
         produtoDescXpath = f"(//div[contains(@class,'ui-widget-content')]/div[1])[{produtoIndex+1}]"
         produtoQtyXpath = f"(//div[contains(@class,'ui-widget-content')]/div[contains(@class, 'CssPes')])[{produtoIndex+1}]"
         produtoValorXpath = f"(//div[contains(@class,'ui-widget-content')]/div[contains(@class, 'CssVlr')])[{produtoIndex+1}]"
@@ -129,6 +133,5 @@ browser.quit()
 #Declarar conteudo //a[@class='Editor btnEditarDeclaracaoConteudo']
 #descrição //div[contains(@class,'ui-widget-content')]/div[1]
 #Quantidade //div[contains(@class,'ui-widget-content')]/div[contains(@class, 'CssPes')]
-#valor //div[contains(@class,'ui-widget-content')]/div[contains(@class, 'CssVlr')]
 #salvar //button[@id='BtnSalvarDeclaracao']
 #importar: btnImportarArquivo
